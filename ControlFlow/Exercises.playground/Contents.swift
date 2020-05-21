53fadf710f868a01565d61a2ef955caabd19852c
Colin Liu <colinliu@Colins-iMac.local>
2020-05-21 09:37:15 +0800

    complete lession 4

--- ControlFlow/Exercises.playground/Contents.swift
+++ ControlFlow/Exercises.playground/Contents.swift
@@ -6,16 +6,38 @@ import UIKit
 //: ### Exercise 1
 //: For each title in the array print the following string: "title + with Puppets", e.g "Point Break with Puppets!"
 var movieTitleArray = ["Point Break", "The Dark Knight", "Star Wars", "When Harry Met Sally"]
+for movieTitle in movieTitleArray {
+    print("\(movieTitle) with Puppets")
+}
 //: ### Exercise 2
 //: Rent is increasing by 20% this year. Use a for-in loop to apply this increase to each item in the oldMountainViewRent array.  Store the new values in the array newMountainViewRent.
 var oldMountainViewRent:[Double] = [2150, 1775, 2221, 1261, 1325, 2110, 1870]
 var newMountainViewRent = [Double]()
+for oldRent in oldMountainViewRent {
+    newMountainViewRent.append(oldRent * 1.2)
+}
+newMountainViewRent
 //: ### Exercise 3
 //: For each food with a true value, print out "<food>, yum!" For each food with a false value print out, "<food>, yuck!"
 var polarizingFoods = ["Anchovies":true, "Coconut":true, "Cilantro":true, "Liver": false]
+for (food, bool) in polarizingFoods {
+    if bool {
+        print("\(food), yum!")
+    } else {
+        print("\(food), yuck!")
+    }
+}
 //: ### Exercise 4
 //: The Oakland area code is changing from 415 to 510. Replace all occurrences of the area code 415 with 510 in the dictionary below.
 var rapperPhoneNumbers = ["Azealia Banks":"(212)548-8777", "Boots Riley":"(415)755-9887", "MC Hammer":"(415)533-9899", "Missy Elliot":"(757)488-5552", "Shock G":"(415)499-7676", "Sir Mix-a-lot":"(206)123-4567", "Snoop Dogg":"(213)760-6664"]
+for (rapper, number) in rapperPhoneNumbers {
+    if number.contains("(415)") {
+        let newNumber = number.replacingOccurrences(of: "(415)", with: "(510)")
+        rapperPhoneNumbers[rapper] = newNumber
+    }
+}
+rapperPhoneNumbers
+
 //: ## Switch Statements
 //: Translate the following if-else statements into switch statements. Feel free to modify print statements as desired.
 //: ### Exercise 5
@@ -24,7 +46,7 @@ enum Sport {
     case baseball, basketball, football, hockey, soccer
 }
 
-var sport = Sport.baseball
+var sport = Sport.soccer
 
 if sport == .baseball {
     print("Go A's!")
@@ -37,6 +59,20 @@ if sport == .baseball {
 } else if sport == .soccer {
     print("Go Earthquakes!")
 }
+
+switch sport {
+case .baseball:
+    print("Go A's!")
+case .basketball:
+    print("Go Warriors!")
+case .football:
+    print( "Go Raiders!")
+case .hockey:
+    print("Go Sharks!")
+case .soccer:
+    print("Go Earthquakes!")
+}
+
 //: ### Exercise 6
 //: Below is an enum and an if-else statement for implementing the game Rock-Paper-Scissors.
 enum roshamboMove {
@@ -58,6 +94,7 @@ if myMove == .rock && yourMove == .paper || myMove == .paper && yourMove == .roc
     resultsMessage = "Scissors cut Paper."
 }
 
+
 //: ### Exercise 7
 //: Below is an if-else statement matching an assignment score to a letter grade.
 var score = 97
@@ -74,10 +111,24 @@ if 90...100 ~= score {
 } else {
     letterGrade = "Incomplete"
 }
+
+switch score {
+case 90...100:
+    letterGrade = "A"
+    case 80...89:
+    letterGrade = "B"
+    case 70...79:
+    letterGrade = "C"
+    case 60...69:
+    letterGrade = "D"
+default:
+    letterGrade = "Incomplete"
+}
+
 //: ### Exercise 8
 //: The if-else statement below translates a word into Pig Latin. Without using the "vowels" array, write an equivalent switch statement.
 
-var word = "can"
+var word = "iPhone"
 var firstLetter = word[word.startIndex]
 var newWord = ""
 var vowels: [Character] = ["a", "e", "i", "o", "u"]
@@ -85,6 +136,14 @@ var vowels: [Character] = ["a", "e", "i", "o", "u"]
 if vowels.contains(firstLetter) {
     newWord = word + "yay"
 } else {
+    word.remove(at: word.startIndex)
+    newWord = "\(word)\(firstLetter)ay"
+}
+
+switch firstLetter {
+case "a", "e", "i", "o", "u":
+    newWord = word + "yay"
+default:
     word.remove(at: word.startIndex)
     newWord = "\(word)\(firstLetter)ay"
 }
